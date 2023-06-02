13a70b3b3cf1de503c1e404495a79a5f8835aecc
Luz Stella Soto G <lssotog@gmail.com>
2023-06-01 20:01:01 -0500

    NewPost component - modal

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,11 @@
+import NewPost from "./NewPost";
 import { Post } from "./Post";
 import classes from "./PostList.module.css";
 
 export const PostList = () => {
   return (
     <>
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="Misito" body="React.js is awesome!" />
         <Post author="Luz Stella" body="Amazing!" />
