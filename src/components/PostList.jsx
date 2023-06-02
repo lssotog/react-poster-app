632a6603a6711a7c85da1c095cb2ec123ce911a0
Luz Stella Soto G <lssotog@gmail.com>
2023-06-01 22:00:37 -0500

    Lifting State Up

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import { Post } from "./Post";
 import classes from "./PostList.module.css";
 
 export const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("React.js is great!");
+  const [enteredAuthor, setEnteredAuthor] = useState("Misito");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function AuthorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={AuthorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Misito" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Luz Stella" body="Amazing!" />
       </ul>
     </>
